847d52b091cbdbcc65bfdbe89d3d15b0fa0a3d57
Your Name <you@example.com>
2026-01-30 13:51:49 +0100

    Afegir atributs i constructor

---
+++ src/Entitat.java
@@ -0,0 +1,13 @@
+public class Entitat {
+    private String nom ;
+    private int nivell;
+    private int puntsVida;
+
+    public Entitat(String nom, int nivell, int puntsVida) {
+        this.nom = nom;
+        this.nivell = nivell;
+        this.puntsVida = puntsVida;
+    }
+
+
+}
