cbc952de9d2b2cb6e6a705be5cfda3bbb732e8e7
Your Name <you@example.com>
2026-02-06 12:47:17 +0100

    Definir classe mag amb el seu metode

---
+++ src/Ma.java
@@ -0,0 +1,16 @@
+public class Ma extends Entitat {
+    private int puntsMagia;
+
+    public Ma(String nom, int nivell, int vida, int mana) {
+        super(nom, nivell, vida);
+        this.puntsMagia = puntsMagia;
+    }
+    public void atacar() {
+        System.out.println(getNom() + " llança un encanteri màgic usant els seus punts de màgia!");
+    }
+
+
+    public int getPuntsMagia() { return puntsMagia; }
+    public void setPuntsMagia(int puntsMagia) { this.puntsMagia = puntsMagia; }
+}
+
