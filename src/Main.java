c18c4d63ebed22482a028b62a17831cf0787abf7
Your Name <you@example.com>
2026-02-06 13:16:57 +0100

    Iniciar bucle de combat

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         ArrayList<Entitat> personatges = new ArrayList<>();
         Guerrer g = new Guerrer("Jon Snow", 1, 100, 20);
         g.afegirCarta("Atac del tren malalt (-5 vida)");
-
+        personatges.add(g);
+        personatges.add(new Ma("Gandi", 1, 80, 100));
+        personatges.add(new Monstre("Godzilla", 1, 50));
+        System.out.println("=== INICI DE LA BATALLA ===");
+        for (Entitat e : personatges) {
+            e.atacar();
+        }
     }
 }
