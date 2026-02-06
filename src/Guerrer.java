8fc2bdf95c55c5ad2c4d45afa5bed507c730d942
Your Name <you@example.com>
2026-02-06 12:45:53 +0100

    Definir metode atacar classe guerrer

--- src/Guerrer.java
+++ src/Guerrer.java
@@ -1,13 +1,14 @@
 public class Guerrer extends Entitat {
-    private int resistencia;
 
-    public Guerrer(String nom, int nivell, int vida, int resistencia) {
-        super(nom, nivell, vida);
-        this.resistencia = resistencia;
+
+    public Guerrer(int nivell, String nom, int vida) {
+        super(nom ,nivell,vida);
+
+
     }
 
     public void atacar() {
-        System.out.println(getNom() + " ataca amb força física!");
+        System.out.println(getNom() + " atacar!");
     }
 }
 
