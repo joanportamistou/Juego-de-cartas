1f8f309a8ac1f0c60a31f235e08a3ac80640c108
Your Name <you@example.com>
2026-02-06 12:41:41 +0100

    Acabar de definir metodes clase principal

--- src/Entitat.java
+++ src/Entitat.java
@@ -1,4 +1,4 @@
-public class Entitat {
+public abstract class Entitat {
     private String nom ;
     private int nivell;
     private int puntsVida;
@@ -8,6 +8,34 @@ public class Entitat {
         this.nivell = nivell;
         this.puntsVida = puntsVida;
     }
+    public String getNom() { return nom; }
+
+    public int getNivell() { return nivell; }
+
+    public void setNivell(int n) {
+
+        if (n == this.nivell + 1) {
+            this.nivell = n;
+        } else {
+            System.out.println("ERROR: El nivell només pot pujar d'1 en 1.");
+        }
+    }
+
+    public int getPuntsVida() { return puntsVida; }
+
+    public void setPuntsVida(int v) {
+
+        this.puntsVida = Math.max(v, 0);
+    }
 
 
+    public void rebreDany(int quantitat) {
+        setPuntsVida(this.puntsVida - quantitat);
+        System.out.println(nom + " ha rebut " + quantitat + " de dany. Vida restant: " + puntsVida);
+    }
+
+
+    public abstract void atacar();
 }
+
+
