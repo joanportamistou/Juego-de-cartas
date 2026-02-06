6ffe9e8e231339baa1c96b257175202e9f4939e4
Your Name <you@example.com>
2026-02-06 13:12:59 +0100

    Definir ArrayList per a llista de personatjes participants

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Entitat> personatges = new ArrayList<>();
 
 
     }
-}
