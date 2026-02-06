5c26ba8dc84eb0b65dfbb68a3c16033ed0335976
Your Name <you@example.com>
2026-02-06 13:36:13 +0100

    Inplementar tipus a Mag

--- src/Ma.java
+++ src/Ma.java
@@ -2,11 +2,11 @@ public class Ma extends Entitat {
     private int puntsMagia;
 
     public Ma(String nom, int nivell, int vida, int mana) {
-        super(nom, nivell, vida);
+        super(nom, nivell, vida,Raresa.MAGIC);
         this.puntsMagia = puntsMagia;
     }
     public void atacar() {
-        System.out.println(getNom() + " llança un encanteri màgic usant els seus punts de màgia");
+        System.out.println(getNom() + " llança un encanteri màgic usant els seus punts de màgia Dany:" +Raresa.MAGIC );
         usarCartes();
     }
 
