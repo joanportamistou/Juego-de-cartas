8b7195a86d2562dc875fa7f7c378a98902dc82c2
Your Name <you@example.com>
2026-02-06 13:33:53 +0100

    Inplementar tipus de dany

--- src/Guerrer.java
+++ src/Guerrer.java
@@ -2,13 +2,13 @@ public class Guerrer extends Entitat {
 
 
     public Guerrer(int nivell, String nom, int vida) {
-        super(nom ,nivell,vida);
+        super(nom ,nivell,vida,Raresa.FISIC);
 
 
     }
     @Override
     public void atacar() {
-        System.out.println(getNom() + " atacar!");
+        System.out.println(getNom() + " atacar! Dany: " + Raresa.FISIC);
         usarCartes();
     }
 
