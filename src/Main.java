7ec3a85ade0051cc9e5d899d457bb79e7d2406a9
Your Name <you@example.com>
2026-02-06 13:14:35 +0100

    Creació Personatge

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         ArrayList<Entitat> personatges = new ArrayList<>();
-
+        Guerrer g = new Guerrer("Jon Snow", 1, 100, 20);
+        g.afegirCarta("Atac del tren malalt (-5 vida)");
 
     }
+}
